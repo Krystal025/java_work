61cb6e6b6caf9b8e860527855e40008fa0e479e2
sujeong <sevols@naver.com>
2023-05-10 12:12:53 +0900

    Step14 Frame06 추가

--- Step14_Gui/src/frame05/MyFrame.java
+++ Step14_Gui/src/frame05/MyFrame.java
@@ -1,37 +1,43 @@
 package frame05;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
-public class MyFrame extends JFrame{
+public class MyFrame extends JFrame implements ActionListener{
+	//필드
+	JButton sendBtn;
+	JButton deleteBtn;
+	JButton updateBtn;	
+	
 	//생성자
 	public MyFrame(String title) {
 		super(title);
 		
-		//setBounds(x, y, width, height) 창의 위치와 크기 설정
 		this.setBounds(100, 100, 500, 500);
-		//프레임(MyFrame)의 x버튼(close버튼)을 눌렀을 떄 프로세스도 같이 종료되도록 설정
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//레이아웃 매니저는 아직 사용하지 않을 것(모든 UI를 절대 좌표에 배치하겠
+		setLayout(new FlowLayout());
 		
-		//FlowLayout 매니저 사용해보기
-		setLayout(new BorderLayout());
-		//프레임에 버튼 추가하기
+		//전송버튼의 참조값을 필드에 저장 (this.은 생략가능) 
+		this.sendBtn = new JButton("전송");
+		add(sendBtn);
 		
-		JButton btn1 = new JButton("버튼1");
-		add(btn1, BorderLayout.NORTH);
+		this.deleteBtn = new JButton("삭제");
+		add(deleteBtn);
+
+		this.updateBtn = new JButton("수정");
+		add(updateBtn);
 		
-		JButton btn2 = new JButton("버튼2");
-		add(btn2, BorderLayout.EAST); //프레임에 버튼추가 
-		
-		JButton btn3 = new JButton("버튼3");
-		add(btn3, BorderLayout.SOUTH); //프레임에 버튼추가 
-		
-		JButton btn4 = new JButton("버튼4");
-		add(btn4, BorderLayout.WEST); //프레임에 버튼추가
+		//전송, 삭제, 수정 버튼 모두 다 하나의 리스너 등록하기 
+		sendBtn.addActionListener(this);
+		deleteBtn.addActionListener(this);
+		updateBtn.addActionListener(this);
 		
 		//프레임을 화면상에 실제로 보이게 하기 (false하면 보이지 않음)
 		this.setVisible(true);
@@ -40,5 +46,23 @@ public class MyFrame extends JFrame{
 		//MyFrame 객체 생성
 		new MyFrame("나의 프레임");
 		System.out.println("main 메소드가 종료됩니다");
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		//메소드의 매개변수로 전달되는 ActionEvent 객체에 이벤트에 대한 정보가 들어있따
+		//이 객체를 활용하면 어떤 버튼이 눌러졌는지 구별할 수 있다
+		
+		//눌러진 버튼의 참조값 얻어오기
+		Object which = e.getSource();
+		if(which == sendBtn) {
+			//전송버튼을 누른것
+			JOptionPane.showMessageDialog(this, "전송합니다");
+		}
+		if(which == deleteBtn) {
+			JOptionPane.showMessageDialog(this, "삭제합니다");
+		}
+		if(which == updateBtn) {
+			JOptionPane.showMessageDialog(this, "수정합니다");
+		}
 	}
 }
