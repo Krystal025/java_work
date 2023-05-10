c5a45433da62c5c2e640dcfa2bc39927b494a934
sujeong <sevols@naver.com>
2023-05-10 12:54:48 +0900

    Step14 Frame07 추가

--- Step14_Gui/src/frame06/MyFrame.java
+++ Step14_Gui/src/frame06/MyFrame.java
@@ -11,61 +11,37 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 public class MyFrame extends JFrame implements ActionListener{
+	//필드
+		JButton sendBtn;
+		JButton deleteBtn;
+		JButton updateBtn;
 	//생성자
 	public MyFrame(String title) {
 		super(title);
 		
-		//setBounds(x, y, width, height) 창의 위치와 크기 설정
 		this.setBounds(100, 100, 500, 500);
-		//프레임(MyFrame)의 x버튼(close버튼)을 눌렀을 떄 프로세스도 같이 종료되도록 설정
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//레이아웃 매니저는 아직 사용하지 않을 것(모든 UI를 절대 좌표에 배치하겠
-		
-		//FlowLayout 매니저 사용해보기
 		setLayout(new FlowLayout());
-		//프레임에 버튼 추가하기
 		
-		JButton sendBtn = new JButton("전송");
+		//전송버튼의 참조값을 필드에 저장 (this.은 생략가능) 
+		this.sendBtn = new JButton("전송");
 		add(sendBtn);
 		
-		
-		Component a = this;
-		
-		//ActionListener 인터페이스 type의 참조값을 얻어내서
-		ActionListener listener1 = new ActionListener() {
-			//이 리스너를 등록한 UI에 어떤 액션이 발생하면 호출되는 메소드
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				
-				System.out.println("전송버튼을 누름");
-				// JOptionPane.showMessageDialog(프레임의 참조값, 띄울 메세지);
-				// 클래스명.this하면 바깥에 있는 클래스로 생성한 객체의 참조값을 가리킬 수 있음
-				JOptionPane.showMessageDialog(MyFrame.this, "전송합니다!");
-			}
-		};
-		//전송버튼에 등록하기
-		sendBtn.addActionListener(listener1);
-		
-		JButton deleteBtn = new JButton("삭제");
+		this.deleteBtn = new JButton("삭제");
 		add(deleteBtn);
-		
-//		deleteBtn.addActionListener(new ActionListener() {
-//			@Override
-//			public void actionPerformed(ActionEvent e) {
-//				System.out.println("삭제버튼을 누름");
-//			}	
-//		});
-		//ActionListener 인터페이스 타입의 참조값을 함수 형태로 전달가능
-		deleteBtn.addActionListener((e)->{
-			System.out.println("삭제버튼을 누름");
-			//함수형태의 메소드에서 this는 바깥 클래스로 생성한 객체의 참조값을 가리킬 수 있다 
-			JOptionPane.showMessageDialog(this, "삭제합니다");
-		});
 
-		JButton updateBtn = new JButton("수정");
+		this.updateBtn = new JButton("수정");
 		add(updateBtn);
-		//MyFrame을 ActionListener 인터페이스 타입으로 만들수 없을까? 
+		
+		//전송, 삭제, 수정 버튼 모두 다 하나의 리스너 등록하기 
+		sendBtn.addActionListener(this);
+		deleteBtn.addActionListener(this);
 		updateBtn.addActionListener(this);
+		
+		//각각의 버튼에 action 명령을 설정할 수 있다
+		sendBtn.setActionCommand("send");
+		deleteBtn.setActionCommand("delete");
+		updateBtn.setActionCommand("update");
 		
 		//프레임을 화면상에 실제로 보이게 하기 (false하면 보이지 않음)
 		this.setVisible(true);
@@ -77,6 +53,18 @@ public class MyFrame extends JFrame implements ActionListener{
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		JOptionPane.showMessageDialog(this, "수정합니다");
+		//메소드의 매개변수로 전달되는 ActionEvent 객체에 이벤트에 대한 정보가 들어있따
+		//이 객체를 활용하면 어떤 버튼이 눌러졌는지 구별할 수 있다
+		
+		//눌러진 버튼의 참조값 얻어오기
+		String cmd = e.getActionCommand();
+
+		if(cmd.equals("send")) {
+			JOptionPane.showMessageDialog(this, "전송합니다");
+		}else if(cmd.equals("delete")) {
+			JOptionPane.showMessageDialog(this, "삭제합니다");
+		}else if(cmd.equals("update")) {
+			JOptionPane.showMessageDialog(this, "수정합니다");
+		}
 	}
 }
---
+++ Step14_Gui/src/frame07/MyFrame.java
@@ -0,0 +1,72 @@
+package frame07;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class MyFrame extends JFrame implements ActionListener {
+	// 생성자
+	public MyFrame(String title) {
+		super(title);
+
+		this.setBounds(100, 100, 500, 500);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(new FlowLayout());
+
+		// 전송버튼의 참조값을 필드에 저장 (this.은 생략가능)
+		JButton sendBtn = new JButton("전송");
+		add(sendBtn);
+
+		JButton deleteBtn = new JButton("삭제");
+		add(deleteBtn);
+
+		JButton updateBtn = new JButton("수정");
+		add(updateBtn);
+
+		// 전송, 삭제, 수정 버튼 모두 다 하나의 리스너 등록하기
+		sendBtn.addActionListener(this);
+		deleteBtn.addActionListener(this);
+		updateBtn.addActionListener(this);
+
+		// 각각의 버튼에 action 명령을 설정할 수 있다
+		sendBtn.setActionCommand("send");
+		deleteBtn.setActionCommand("delete");
+		updateBtn.setActionCommand("update");
+
+		// 프레임을 화면상에 실제로 보이게 하기 (false하면 보이지 않음)
+		this.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		// MyFrame 객체 생성
+		new MyFrame("나의 프레임");
+		System.out.println("main 메소드가 종료됩니다");
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// 메소드의 매개변수로 전달되는 ActionEvent 객체에 이벤트에 대한 정보가 들어있따
+		// 이 객체를 활용하면 어떤 버튼이 눌러졌는지 구별할 수 있다
+
+		// 눌러진 버튼의 참조값 얻어오기
+		String cmd = e.getActionCommand();
+		/*
+		 * JAVA에선 문자열의 참조갑이 같은지 비교할 때에는 비교연산자 ==를 이용해서 비교하지만 문자열의 참조값이 아닌 내용이 같은지 비교할 때는
+		 * String의 .equals()메소드를 이용해서 비교한다 왜냐하면 JAVA에서는 문자열을 만드는 방법에 따라서 내용은 같지만 참조값이
+		 * 다르게 나오는 경우도 있기 때문이다 따라서 문자열의 내용이 같은지 정확히 비교할 때는 반드시 .equals()메소드를 이용해야한다
+		 */
+		if (cmd.equals("send")) {
+			JOptionPane.showMessageDialog(this, "전송합니다");
+		} else if (cmd.equals("delete")) {
+			JOptionPane.showMessageDialog(this, "삭제합니다");
+		} else if (cmd.equals("update")) {
+			JOptionPane.showMessageDialog(this, "수정합니다");
+		}
+	}
+}
