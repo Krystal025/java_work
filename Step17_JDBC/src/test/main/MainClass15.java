ee3e98961464924bc3fcd33f347e48cf7dbf6f28
sujeong <sevols@naver.com>
2023-05-16 11:54:56 +0900

    Step17 DAO Update, Delete 추가

---
+++ Step17_JDBC/src/test/main/MainClass15.java
@@ -0,0 +1,22 @@
+package test.main;
+
+import test.dao.MemberDao;
+import test.dto.MemberDto;
+
+public class MainClass15 {
+	public static void main(String[] args) {
+		//삭제할 회원의 정보
+		int num = 2;
+		
+		//위의 정보를 이용해서 회원정보를 삭제하고 성공이면 "삭제했습니다" 출력
+		
+		MemberDto dto = new MemberDto();
+		dto.setNum(num);
+		
+		MemberDao dao = new MemberDao();
+		boolean isSuccess = dao.delete(num);
+		if(isSuccess) {
+			System.out.println("삭제했습니다");
+		}
+	}
+}
